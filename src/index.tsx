ad5b74561f2f40a57a5979eaf120fd7f242b00eb
olegpl1993 <olegpl1993@gmail.com>
2023-05-25 20:24:49 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import './nullstyle.scss';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import RenderSchema from './components/RenderSchema';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
+    <RenderSchema />
+  </BrowserRouter>
+);
---
+++ src/types.ts
@@ -0,0 +1,82 @@
+import { GraphQLObjectType, GraphQLInputObjectType } from 'graphql';
+
+export type DocumentationRickAndMorty = {
+  type: string;
+  selectedType: string;
+  title: string;
+};
+
+export type GraphQLObject = GraphQLObjectType | GraphQLInputObjectType;
+
+export type GrapQlLNestedNonNull = {
+  name: string;
+  args: {
+    name: string;
+    description: string;
+    type: {
+      kind: string;
+      name: string;
+      ofType: {
+        kind: string;
+        name: string;
+        ofType: {
+          kind: string;
+          name: string;
+          ofType: {
+            kind: string;
+            name: string;
+            ofType: null;
+          };
+        };
+      };
+      defaultValue: null;
+    };
+  };
+  type: {
+    kind: string;
+    name: string;
+    ofType: {
+      kind: string;
+      name: string;
+      ofType: {
+        kind: string;
+        name: string;
+        ofType: {
+          kind: string;
+          name: string;
+          ofType: null;
+        };
+      };
+    };
+    defaultValue: null;
+  };
+  description: string;
+  isDeprecated: false;
+  deprecationReason: null;
+};
+
+export type GraphQLNestedList = {
+  name: string;
+  args: [GrapQlLNestedNonNull];
+  type: {
+    kind: string;
+    name: string;
+    ofType: {
+      kind: string;
+      name: string;
+      ofType: {
+        kind: string;
+        name: string;
+        ofType: {
+          kind: string;
+          name: string;
+          ofType: null;
+        };
+      };
+    };
+    defaultValue: null;
+  };
+  description: string;
+  isDeprecated: false;
+  deprecationReason: null;
+};
