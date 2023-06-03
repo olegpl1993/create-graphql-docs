4d59bf408df3d81453905a87c9669901f7c68d8d
olegpl1993 <olegpl1993@gmail.com>
2023-06-03 14:23:35 +0300

    feat: filter base types

--- index.tsx
+++ index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Docs from './Docs';
 
+// https://rickandmortyapi.com/graphql
+// https://countries.trevorblades.com/graphql
+// https://graphqlpokemon.favware.tech/
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Docs url="https://rickandmortyapi.com/graphql" />
 );
--- src/RenderDocs.tsx
+++ src/RenderDocs.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { IntrospectionObjectType, IntrospectionSchema } from 'graphql';
+import { IntrospectionObjectType, IntrospectionSchema, IntrospectionType } from 'graphql';
 import requestSchema from './funcs/requestSchema';
 import Queries from './components/Queries';
 import Types from './components/Types';
@@ -16,6 +16,7 @@ function RenderDocs(props: Props) {
   const [openQueries, setOpenQueries] = useState<boolean>(false);
   const [schema, setSchema] = useState<IntrospectionSchema | null>(null);
   const queryType = schema?.types.find(({ name }) => name === 'Query');
+  const mainTypes = schema?.types.filter(({ name }) => name !== 'Query' && !name.startsWith('__'));
 
   useEffect(() => {
     (async () => {
@@ -36,7 +37,7 @@ function RenderDocs(props: Props) {
         </button>
         {openTypes && (
           <div className="docs-nested">
-            <Types types={schema.types} />
+            <Types types={mainTypes as ReadonlyArray<IntrospectionType>} />
           </div>
         )}
         <button
@@ -45,7 +46,7 @@ function RenderDocs(props: Props) {
             setOpenQueries(!openQueries);
           }}
         >
-          Queries
+          Query
         </button>
         {openQueries && (
           <div className="docs-nested">
